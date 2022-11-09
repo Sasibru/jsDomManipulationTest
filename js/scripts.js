e21ea05357b39adc2c75111d94e3897eed294b8d
Sander Brustad <sander.simonsen.brustad@gmail.com>
2022-11-09 12:58:28 +0100

    Add WIP to css js test

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,24 @@
 
 const container = document.querySelector('#container');
 
+const redText = document.createElement("p");
+redText.textContent = "Hey Im red!";
+redText.style.color = "red";
+container.appendChild(redText);
+
+
+const blueH3 = document.createElement("h3");
+blueH3.textContent = "I'm a blue h3!";
+blueH3.style.color = "blue";
+container.appendChild(blueH3);
+
+const div = document.createElement("div");
+div.classList.add("borderDiv");
+div.classList.toggle('active');
+const h1 = document.createElement("h1");
+div.appendChild(h1);
+const appendedP = document.createElement("p");
+div.appendChild(appendedP);
+h1.textContent = "I'm in a div";
+appendedP.textContent = "ME TOO!";
+container.appendChild(div);
